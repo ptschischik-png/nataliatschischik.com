86573a2ac655edeb6a5d530b54a2778dda741848
ptschischik-png <p.tschischik@googlemail.com>
2026-04-19 08:31:16 +0200

    Bad Hersfeld

--- src/_data/site.cjs
+++ src/_data/site.cjs
@@ -85,6 +85,7 @@ site.schema = {
     { '@type': 'City', name: 'Marburg' },
     { '@type': 'City', name: 'Gießen' },
     { '@type': 'City', name: 'Fulda' },
+    { '@type': 'City', name: 'Bad Hersfeld' },
     { '@type': 'AdministrativeArea', name: 'Nordhessen' },
     { '@type': 'AdministrativeArea', name: 'Hessen' },
     { '@type': 'AdministrativeArea', name: 'Niedersachsen' }
@@ -105,7 +106,7 @@ site.schema = {
         '@id': site.url + '/#business',
         name: site.contact.businessName,
         alternateName: site.contact.displayName,
-        description: 'Hochzeitsfotografie in Kassel, Göttingen, Marburg, Gießen, Fulda und ganz Hessen mit dokumentarischen, zeitlosen Reportagen.',
+        description: 'Hochzeitsfotografie in Kassel, Göttingen, Marburg, Gießen, Fulda, Bad Hersfeld und ganz Hessen mit dokumentarischen, zeitlosen Reportagen.',
         url: site.url,
         image: site.url + '/img/ext/gd-15zAiT52byizm3F.webp',
         logo: site.url + '/favicon.svg',
@@ -119,6 +120,7 @@ site.schema = {
           { '@type': 'City', name: 'Marburg' },
           { '@type': 'City', name: 'Gießen' },
           { '@type': 'City', name: 'Fulda' },
+          { '@type': 'City', name: 'Bad Hersfeld' },
           { '@type': 'AdministrativeArea', name: 'Nordhessen' },
           { '@type': 'AdministrativeArea', name: 'Hessen' },
           { '@type': 'AdministrativeArea', name: 'Niedersachsen' }
